143fa7c872b2ac03fc6688b2383881312358cc13
hedanur <hedanurcakirr@gmail.com>
2026-03-31 14:06:57 +0300

    ex1 bitti ex2' başlandı

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -3,10 +3,14 @@ class Plant:
 		self.age = age
 		self.name = name
 		self.height = height
+def plant_str(p):
+	print(f"{p.name}: {p.height}cm, {p.age} days old")
 
 if __name__ == "__main__":
-	print("=== Garden Plant Registry ===")
 	plant1 = Plant("Rose",25,30)
 	plant2 = Plant("Sunflower",80,45)
 	plant3 = Plant("Cactus",15,120)
-
+	print("=== Garden Plant Registry ===")
+	plant_str(plant1)
+	plant_str(plant2)
+	plant_str(plant3)
