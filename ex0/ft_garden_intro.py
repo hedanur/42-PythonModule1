e72618424dbd49339639dc7e33921e8a50929a3a
hedanur <hedanurcakirr@gmail.com>
2026-03-31 10:53:05 +0300

    ilk iki egzeriz yapıldı

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+name ="Rose"
+age = "30 days"
+height = "25 cm"
+if __name__ == "__main__":
+	print("=== Welcome to My Garden ===")
+	print("Plant:", name)
+	print("Height:" , height)
+	print("Age:", age)
+	print("\n=== End of Program ===")
