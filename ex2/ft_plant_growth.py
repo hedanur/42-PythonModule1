e71556afdb70f2fc303ff45722e93b557109efa7
hedanur <hedanurcakirr@gmail.com>
2026-04-11 18:55:28 +0300

    gitti

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -6,10 +6,31 @@ class Plant:
         self.name = None
         self.height = None
 
-    def grow(self):
+    def show(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
-    def age();
+    def grow(self):
+        self.height = round(self.height + 0.8, 1)
+
+    def age_plant(self):
+        self.age += 1
 
 
 if __name__ == "__main__":
+    rose = Plant()
+    rose.name = "Rose"
+    rose.height = 25.0
+    rose.age = 30
+
     print("=== Garden Plant Growth ===")
+    rose.show()
+    rose.grow()
+    rose.age_plant()
+    i = 1
+    while (i < 8):
+        print(f"=== Day {i} ===")
+        rose.show()
+        rose.grow()
+        rose.age_plant()
+        i += 1
+    print(f"Growth this week: {round(0.8 * 7, 1)}cm")
