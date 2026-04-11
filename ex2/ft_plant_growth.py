e2882e9f593d16f14e2d1d91b30b227cf81185d8
hedanur <hedanurcakirr@gmail.com>
2026-04-11 17:12:35 +0300

    fixed: ex2

---
+++ ex2/ft_plant_growth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self):
+        self.age = None
+        self.name = None
+        self.height = None
+
+    def grow(self):
+
+    def age();
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Growth ===")
