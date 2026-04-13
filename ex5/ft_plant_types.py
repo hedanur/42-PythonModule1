ba171d3d0167166327ffdb6cd90902a7dd4cfd8f
hedanur <hedanurcakirr@gmail.com>
2026-04-13 17:47:31 +0300

    ex5 tamamlandı

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -3,46 +3,67 @@
 class Plant:
     def __init__(self, name, height, age):
         self.name = name
-        self.age = age
+        self._age = age  #_age kullanmak mantıklı mı
         self.height = height
+        self.is_bloom = False
+
+    def grow(self, added_height):
+        self.height += added_height
+
+    def age(self, added_age):
+        self._age += added_age
 
     def show(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
+        print(f"{self.name}: {self.height}cm, {self._age} days old")
 
 
 class Tree(Plant):
-    def __init__(self, name, height, age, trunk_diameter):
-        super().__init__(name, height, age)
+    def __init__(self, name, height, _age, trunk_diameter):
+        super().__init__(name, height, _age)
         self.trunk_diameter = trunk_diameter
 
         print(f"[asking the {self.name} to produce shade]")
 
-
-        #def produce_shade():
+    def show(self):
         super().show()
+        print(f"Trunk diameter: {self.trunk_diameter}")
+
+    def produce_shade(self):
+        print(f"Tree {self.name} now produces a shade of {self.height}\
+ long and {self.trunk_diameter} wide.")
 
 
 class Flower(Plant):
-    def __init__(self, name, height, age, colour):
-        super().__init__(name, height, age)
+    def __init__(self, name, height, _age, colour):
+        super().__init__(name, height, _age)
         self.colour = colour
-        super().show()
 
-        def bloom():
-            self.is_blooming = True
-            if()
-                print(f"{self.name} is blooming beautifully!")
+    def show(self):
+        super().show()
+        print(f" Colour: {rose.colour}")
+
+    def bloom(self):
+        if not self.is_bloom:
+            self.is_bloom = True
+            print(f" {self.name} has not bloomed yet")
+        else:
+            print(f" {self.name} is blooming beautifully!")
 
 
 class Vegetable(Plant):
-    def __init__(self, name, height, age, harvest, nutritional):
-        super().__init__(name, height, age)
+    def __init__(self, name, height, _age, harvest, nutritional):
+        super().__init__(name, height, _age)
         self.harvest = harvest
         self.nutritional = nutritional
-        super().show()
 
-        print(f"Trunk diameter: {self.height}cm")
-        print(f"[make {self.name} grow and age for {self.height} days]")
+    def show(self):
+        super().show()
+        print(f"Harvest season: {self.harvest}")
+        print(f"Nutritional value: {self.nutritional}")
+
+    def age(self, n):
+        self.nutritional += n
+        super().age(n)
 
 
 if __name__ == "__main__":
@@ -50,17 +71,26 @@ if __name__ == "__main__":
 
     print("=== Flower")
     rose = Flower("Rose", 15.0, 10, "red")
-    print(f"Colour: {rose.colour}")
-    print()
+    rose.show()
+    rose.bloom()
+    print("[asking the rose to bloom]")
+    rose.show()
+    rose.bloom()
 
-    print("=== Tree")
+    print("\n=== Tree")
     oak = Tree("Oak", 200.0, 365, 5.0)
-    print()
+    oak.show()
+    oak.produce_shade()
+    
 
-    print("=== Vegetable")
+    print("\n=== Vegetable")
     tomato = Vegetable("Tomato", 5.0, 10, "April", 0)
-    print(f"Harvest season: {tomato.harvest}")
-    print(f"Nutritional value: {tomato.nutritional}")
+    tomato.show()
+    print(f"[make {tomato.name} grow and age for 20 days]")
+    tomato.grow(42)
+    tomato.age(20)
+    tomato.show()
+
 
 
 #"Overriding"
